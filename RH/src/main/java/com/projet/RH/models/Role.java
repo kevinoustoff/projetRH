bd93ecb9dcb0608a72996d9abfa270a1318f0f04
Kevin <garrickervin@gmail.om>
2024-11-21 15:11:21 +0100

    #ajout table offreemploi utilisateur

---
+++ RH/src/main/java/com/projet/RH/models/Role.java
@@ -0,0 +1,37 @@
+package com.projet.RH.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="roles")
+public class Role {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String nom;
+	
+	public Role() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+}
